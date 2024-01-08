5ddfaa86f9f4f6adca2ffa35e2bbb9431fffc826
user <user@192.168.1.129>
2024-01-08 11:49:15 +0900

    2024/1/8

---
+++ src/main/java/katachi/spring/execise/JavaPortforioApplication.java
@@ -0,0 +1,13 @@
+package katachi.spring.execise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPortforioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPortforioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/katachi/spring/execise/JavaPortforioApplicationTests.java
@@ -0,0 +1,13 @@
+package katachi.spring.execise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPortforioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
